9c19ed65e5e2d042caac3ee77e89de4081428248
jhuerta6 <jorgehuertahu@gmail.com>
2017-03-31 23:21:31 -0600

    Necesitamos implementar la nueva imagen y login

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Logistica Aduanera Juarez</title>
+
+  <!-- Bootstrap -->
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.js"></script>
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/custom.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<!--<header>
+  <div id="video_filter">
+    <iframe id="video-bg" frameborder="0" allowfullscreen="1" width="560" height="315" src="https://www.youtube.com/embed/twm_t7U5zkI?autohide=1&autoplay=1&controls=0&enablejsapi=1&iv_load_policy=3&loop=1&modestbranding=1&playsinline=1&rel=0&showinfo=0&wmode=opaque&widgetid=1" style="width: 1349px; height: 758.813px; left: 0px; top: -246.906px;"></iframe>
+  </div>
+  <!--<div class="container">
+    <div class="intro-video">
+        <video id="video" preload autoplay="true" loop="loop" muted="muted" volume="0">
+            <source src="http://s3-eu-west-1.amazonaws.com/docplanner.com/video/timelapse.mp4" type="video/mp4">
+            <source type="video/webm" src="//s3-eu-west-1.amazonaws.com/docplanner.com/video/timelapse.webm"></source>
+        </video>
+    </div>
+  </div>-->
+  
+<body>
+
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Logistica Aduanera</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="#">INICIO</a></li>
+          <li id="custom_hover_nav"><a class="dropdown-toggle" data-toggle="dropdown" href="#">INVENTARIO SUBASTADO<span class="caret"></span></a>
+            <ul class="dropdown-menu" id="collapse">
+              <li><a href="#">Harley Davidson</a></li>
+              <li><a href="#">Motos 2</a></li>
+              <li><a href="#">Motos 3</a></li>
+            </ul></li>
+            <li><a href="#">TERMINOS Y CONDICIONES</a></li>
+            <li><a href="#">CONTACTANOS</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registrate</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <script>
+
+    $(document).ready(function(){
+      /*var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+
+      if (!isMobile.matches) {
+        $("#custom_hover_nav").hover(function(){ //show inventario on hover
+          $("#collapse").collapse('toggle');
+        });
+      }
+      */
+    });
+    </script>
+
+    <!--<h1>Hello, world!</h1>-->
+
+  </body>
